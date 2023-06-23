2dbf0ed5d8a71cb8e6bfceb5cebcdc41e7b655ac
MeGustaDev <gustavoguimaraesreis@gmail.com>
2023-06-23 01:05:47 -0300

    feat: subindo última versão dos códigos

--- main.py
+++ main.py
@@ -28,13 +28,3 @@ tweets_df = pd.DataFrame(tweets_list, columns=['Date', 'TweetId', 'TweetContent'
 
 result = tweets_df.to_json(path_or_buf= file_name, orient='records', force_ascii=False, lines=True)
 
-# tweets_df['Date'] = [d.date() for d in tweets_df['DateTime']]
-# tweets_df['Time'] = [d.time() for d in tweets_df['DateTime']]
-
-# tweets_df.drop('DateTime', axis=1, inplace=True)
-# tweets_df.info()
-# print(tweets_df)
-
-# result = tweets_df.to_json(path_or_buf='resultado.json', orient='records', lines=True, indent=4)
-
-# file_name = input("Qual o nom do arquivo que deseja salvar os resultados? ") # exemplo: resultado.json
--- teste.py
+++
@@ -1,7 +0,0 @@
-lista = []
-
-lista.append([2,4])
-lista.append([6,5])
-lista.append([8,9])
-
-print(lista)
